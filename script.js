e74d70a80bcc65fee3523bb8916193cc172b80e1
Jesús Hernández <jesushdez1303@gmail.com>
2025-02-26 00:12:55 -0400

    finished

--- script.js
+++ script.js
@@ -12,12 +12,13 @@ function initialize(){
     list_selector = document.getElementById("list");
     list = document.getElementById("list");
     storeField = field_value.value;
-    button = document.getElementById("add");
+    button = document.getElementById("add_button");
     counter = document.getElementById("counter");
 }
 
 function addItem(){
     initialize();
+    if(list.children.length < 12){
     if(field_value.value.length > 0){
     const item = document.createElement("li");
     list_selector.appendChild(item);
@@ -45,7 +46,7 @@ function addItem(){
 
     const img = document.createElement("img");
     delete_button.appendChild(img);
-    img.setAttribute("src", "delete.svg"); img.setAttribute("widht","16px"); img.setAttribute("height","16px");
+    img.setAttribute("src", "delete.svg"); img.setAttribute("width","16px"); img.setAttribute("height","16px");
 
     updateCounter();
     field_value.value = "";
@@ -54,6 +55,17 @@ function addItem(){
         alert("Please, enter a task");
     }
 
+ }
+ else{
+    alert("Task limit reached");
+ } 
+ if(list.children.length > 11){
+    counter.classList.remove("counter_available"); counter.classList.add("counter_limit");
+    button.classList.add("unavailable_add_button"); button.classList.remove("add_button")
+    button.disabled = true;
+ }
+    
+
 }
 
 function deleteItem(e){
@@ -61,6 +73,11 @@ function deleteItem(e){
     const current_position = e.currentTarget.number_list;
     document.getElementById(`list_element${current_position}`).remove();
     updateCounter();
+    if(list.children.length < 12){
+        button.disabled = false;
+        counter.classList.remove("counter_limit"); counter.classList.add("counter_available");
+        button.classList.remove("unavailable_add_button"); button.classList.add("add_button")
+    }
 }
 
 function lineText(e){
@@ -80,5 +97,8 @@ function delete_all(){
     for(let index = 1; index <= all_items; index++){
         document.getElementById(`list_element${index}`).remove();
     }
+    counter.classList.remove("counter_limit"); counter.classList.add("counter_available");
+    button.classList.remove("unavailable_add_button"); button.classList.add("add_button");
+    button.disabled = false;
     updateCounter();
 }
